b455050a4036c075389dff677d596dd7d1147bed
Martin Fetzel <martin.fetzel@get-the-solution.net>
2021-02-21 13:40:57 +0100

    get rid of db-4.7.25

--- db-4.7.25/java/src/com/sleepycat/db/ForeignKeyNullifier.java
+++
@@ -1,6 +0,0 @@
-package com.sleepycat.db;
-
-public interface ForeignKeyNullifier {
-    boolean nullifyForeignKey(SecondaryDatabase secondary, DatabaseEntry data)
-	    throws DatabaseException;
-}
--- db-4.7.25/java/src/com/sleepycat/db/ForeignMultiKeyNullifier.java
+++
@@ -1,6 +0,0 @@
-package com.sleepycat.db;
-
-public interface ForeignMultiKeyNullifier {
-    boolean nullifyForeignKey(SecondaryDatabase secondary, DatabaseEntry key, DatabaseEntry data, DatabaseEntry secKey)
-	    throws DatabaseException;
-}
